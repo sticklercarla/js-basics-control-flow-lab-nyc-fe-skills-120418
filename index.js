c503dc88e87b0495ec422069b07a053ecc85c401
Carla Stickler <carlastickler@gmail.com>
2019-01-18 00:32:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     return ("This one is on me!")
   } else if (someValue >= 2000) {
-    return ("I will gladly take your 30 bucks.")
+    return ("I will gladly take your thirty bucks.")
   }
   } 
