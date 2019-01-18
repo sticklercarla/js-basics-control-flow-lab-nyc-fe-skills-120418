f5ab16deca4b18a8894770ecff488da0dd8a8e80
Carla Stickler <carlastickler@gmail.com>
2019-01-18 00:52:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,7 @@ function scuberGreetingForFeet(someValue) {
 function ternaryCheckCity (someCity) {
    return (someCity === "NYC") ?  ("Ok, sounds good.") : ("No go.")
   }
+  function switchOnCharmFromTip(someTip) {
+    
+  }
 
