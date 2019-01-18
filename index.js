1091b65cbe45ddb37fb90e0b0768750194126962
Carla Stickler <carlastickler@gmail.com>
2019-01-18 01:09:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return ("This one is on me!")
+  } else if (someValue >= 2500) {
+    return ("No can do.")
+  } else {
+    return ("I will gladly take your thirty bucks.")
+  }
+  } 
+
+function ternaryCheckCity (someCity) {
+   return (someCity === "NYC") ?  ("Ok, sounds good.") : ("No go.")
+  }
+  
+function switchOnCharmFromTip(someTip) {
+  switch(someTip) {
+    case "generous": return "Thank you so much.";
+    break;
+    case "not as generous": return "Thank you.";
+    break;
+    default: return "Bye.";
+  }
+}
+
